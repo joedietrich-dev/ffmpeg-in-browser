283da871fbbe7106043d6c8a5c7afbb5f7ca5734
Joe Dietrich <joe@joedietrich.net>
2021-06-16 22:47:55 -0400

    Stop loading ffmpeg twice

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const { createFFmpeg, fetchFile } = FFmpeg;
 const ffmpeg = createFFmpeg({ log: true });
 const transcode = async ({ target: { files } }) => {
   const { name } = files[0];
-  await ffmpeg.load();
+  if (!ffmpeg.isLoaded()) await ffmpeg.load();
   ffmpeg.FS('writeFile', name, await fetchFile(files[0]));
   await ffmpeg.run('-i', name, '-c:v', 'libx264', '-preset', 'slow', '-crf', '25', '-vf', 'scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:-1:-1:color=black', '-c:a', 'copy', 'output.mp4');
   const data = ffmpeg.FS('readFile', 'output.mp4');
